ca829020c52f3cc977e7c7678f87d75eda4d139e
Cory Coward <cory@c2softwaresolutions.com>
2020-09-25 14:37:08 -0500

    Add pagination to listings page

--- listings/views.py
+++ listings/views.py
@@ -7,6 +7,8 @@ from .models import Listing
 class ListingListView(ListView):
 
     model = Listing
+    paginate_by = 6
+    ordering = ['-created_on', ]
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
