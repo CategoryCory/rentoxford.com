92b9f142225d7c4019cd33abdcac72899df861cc
Cory Coward <cory@c2softwaresolutions.com>
2020-09-25 15:42:06 -0500

    Change user fields and user page in admin

--- users/admin.py
+++ users/admin.py
@@ -15,7 +15,12 @@ class CustomUserAdmin(UserAdmin):
     list_editable = ['is_approved', ]
     list_filter = ['is_approved', ]
     list_per_page = 25
-    # fieldsets = ()
+    fieldsets = (
+        ('Account and Personal Information', {'fields': ('username', 'email', 'first_name', 'last_name', )}),
+        ('Tenant Information', {'fields': ('property', 'monthly_rent', )}),
+        ('Permissions', {'fields': ('is_active', 'is_approved', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Account Login Information', {'fields': ('last_login', 'date_joined', )}),
+    )
 
 
 admin.site.register(CustomUserModel, CustomUserAdmin)
--- users/models.py
+++ users/models.py
@@ -2,16 +2,16 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
 
+from listings.models import Listing
+
 
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(default=timezone.now)
     is_approved = models.BooleanField(verbose_name='Approved',
                                       help_text='Designates whether this user has been approved.',
                                       default=False)
-    address_street = models.CharField(verbose_name='Street Address', max_length=200)
-    address_city = models.CharField(verbose_name='City', max_length=75)
-    address_state = models.CharField(verbose_name='State', max_length=50)
-    address_zip = models.CharField(verbose_name='ZIP Code', max_length=50)
+    property = models.ForeignKey(Listing, on_delete=models.SET_NULL, null=True, blank=True)
+    monthly_rent = models.DecimalField(max_digits=8, decimal_places=2, default=0)
 
     def __str__(self):
         return self.username
