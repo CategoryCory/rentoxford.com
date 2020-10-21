21a7b3baf9c5a4c7dc8a96c66b91818fe81f5179
Cory Coward <cory@c2softwaresolutions.com>
2020-10-20 20:36:32 -0500

    Add Lease Charge and Lease Payment models

--- leases/admin.py
+++ leases/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Company, Lease, LeaseDocument
+from .models import Company, Lease, LeaseDocument, LeaseCharge, LeasePayment
 
 
 class LeaseDocumentInline(admin.TabularInline):
@@ -9,8 +9,8 @@ class LeaseDocumentInline(admin.TabularInline):
 
 
 class LeaseAdmin(admin.ModelAdmin):
-    list_display = ['tenant', 'property', 'lease_begin', 'lease_end', 'monthly_rent', ]
-    search_fields = ['tenant', 'property', ]
+    list_display = ['property', 'company', 'lease_begin', 'lease_end', 'monthly_rent', ]
+    search_fields = ['property', 'company', ]
     list_per_page = 25
     inlines = [
         LeaseDocumentInline,
@@ -22,5 +22,19 @@ class CompanyAdmin(admin.ModelAdmin):
     list_per_page = 25
 
 
+class LeaseChargeAdmin(admin.ModelAdmin):
+    list_display = ['tenant', 'due_date', 'amount', 'balance', 'status', 'notes', ]
+    list_filter = ['tenant', 'due_date', ]
+    list_per_page = 25
+
+
+class LeasePaymentAdmin(admin.ModelAdmin):
+    list_display = ['tenant', 'date', 'amount', 'balance', 'notes', ]
+    list_filter = ['tenant', ]
+    list_per_page = 25
+
+
 admin.site.register(Lease, LeaseAdmin)
 admin.site.register(Company, CompanyAdmin)
+admin.site.register(LeaseCharge, LeaseChargeAdmin)
+admin.site.register(LeasePayment, LeasePaymentAdmin)
--- leases/models.py
+++ leases/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils import timezone
+from calendar import monthrange
+import datetime
 
 from listings.models import Listing
 UserModel = get_user_model()
@@ -23,7 +25,6 @@ class Company(models.Model):
 
 
 class Lease(models.Model):
-    tenant = models.ForeignKey(UserModel, on_delete=models.CASCADE)
     property = models.ForeignKey(Listing, on_delete=models.CASCADE)
     company = models.ForeignKey(Company, on_delete=models.SET_NULL, null=True, blank=True)
     lease_begin = models.DateField(default=timezone.now)
@@ -33,13 +34,60 @@ class Lease(models.Model):
     notes = models.TextField(blank=True)
 
     def __str__(self):
-        return f'{self.tenant.first_name} {self.tenant.last_name}: {self.lease_begin}-{self.lease_end}'
+        return f'{self.property}: {self.lease_begin} - {self.lease_end}'
 
 
 class LeaseDocument(models.Model):
     lease = models.ForeignKey(Lease, on_delete=models.CASCADE)
+    description = models.CharField(max_length=255)
     lease_document = models.FileField(upload_to='documents/%Y/%m/%d/', verbose_name='Lease Document')
 
     class Meta:
         verbose_name = 'Lease Document'
         verbose_name_plural = 'Lease Documents'
+
+    def __str__(self):
+        return f'{self.lease} - {self.description}'
+
+
+class LeaseCharge(models.Model):
+    DUE = 'due'
+    PAID = 'paid'
+    LATE = 'late'
+
+    STATUS_CHOICES = (
+        (DUE, 'Due'),
+        (PAID, 'Paid'),
+        (LATE, 'Late'),
+    )
+
+    tenant = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    due_date = models.DateField(default=timezone.now)
+    amount = models.IntegerField(default=0)
+    balance = models.IntegerField(default=0)
+    status = models.CharField(max_length=25, choices=STATUS_CHOICES, default=DUE)
+    parent_charge = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
+    notes = models.CharField(max_length=255, blank=True)
+
+    class Meta:
+        verbose_name = 'Lease Charge'
+        verbose_name_plural = 'Lease Charges'
+
+    def __str__(self):
+        return f'{self.tenant.first_name} {self.tenant.last_name} - {self.due_date}'
+
+
+class LeasePayment(models.Model):
+    tenant = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+    amount = models.IntegerField(default=0)
+    balance = models.IntegerField(default=0)
+    notes = models.CharField(max_length=255, blank=True)
+    charges = models.ManyToManyField(LeaseCharge, related_name='lease_payments')
+
+    class Meta:
+        verbose_name = 'Lease Payment'
+        verbose_name_plural = 'Lease Payments'
+
+    def __str__(self):
+        return f'{self.tenant.first_name} {self.tenant.last_name} - {self.date}'
