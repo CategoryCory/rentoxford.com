fd25b63cd9cef2d63b5ba74b5e165fe37ce7e8f2
Cory Coward <cory@c2softwaresolutions.com>
2020-11-27 21:31:53 -0600

    Set up table on admin dashboard tenants page

--- admin_dashboard/views.py
+++ admin_dashboard/views.py
@@ -1,12 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
 from listings.models import Listing
+CustomUser = get_user_model()
 
 
 @login_required
 def ad_home(request):
-    if request.user.is_staff is False:
+    if not request.user.is_staff:
         return redirect('pages:home')
     else:
         listings = Listing.objects.all()
@@ -18,7 +20,11 @@ def ad_home(request):
 
 @login_required
 def ad_tenants(request):
-    if request.user.is_staff is False:
+    if not request.user.is_staff:
         return redirect('pages:home')
     else:
-        return render(request, 'admin_dashboard/ad_tenants.html')
+        tenants = CustomUser.objects.filter(is_staff=False)
+        context = {
+            'tenants': tenants
+        }
+        return render(request, 'admin_dashboard/ad_tenants.html', context)
