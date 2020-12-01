33c032883c1db4dfd259e0b268433510ed5f11dc
Cory Coward <cory@c2softwaresolutions.com>
2020-11-30 21:08:14 -0600

    Add offline payments to admin dashboard

--- admin_dashboard/urls.py
+++ admin_dashboard/urls.py
@@ -6,4 +6,5 @@ app_name = 'admin_dashboard'
 urlpatterns = [
     path('', views.ad_home, name='ad_home'),
     path('tenants/', views.ad_tenants, name='ad_tenants'),
+    path('tenants/<int:tenant_id>/add-payment/', views.AddTenantPaymentView.as_view(), name='ad_add_tenant_payment'),
 ]
--- admin_dashboard/views.py
+++ admin_dashboard/views.py
@@ -1,9 +1,22 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, AccessMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views.generic import CreateView
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+
+from transactions.models import Payment
 
 from listings.models import Listing
 CustomUser = get_user_model()
+
+
+class RedirectIfNotStaffMixin(AccessMixin):
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_staff:
+            return redirect('pages:home')
+        return super(RedirectIfNotStaffMixin, self).dispatch(request, *args, **kwargs)
 
 
 @login_required
@@ -28,3 +41,25 @@ def ad_tenants(request):
             'tenants': tenants
         }
         return render(request, 'admin_dashboard/ad_tenants.html', context)
+
+
+class AddTenantPaymentView(LoginRequiredMixin, RedirectIfNotStaffMixin, SuccessMessageMixin, CreateView):
+    model = Payment
+    fields = ['amount', 'notes', ]
+    template_name = 'admin_dashboard/ad_add_tenant_payment.html'
+    success_url = reverse_lazy('admin_dashboard:ad_tenants')
+    success_message = 'The payment has been successfully recorded.'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        current_tenant = CustomUser.objects.get(pk=self.kwargs['tenant_id'])
+        context['tenant_name'] = f'{current_tenant.first_name} {current_tenant.last_name}'
+        return context
+
+    def form_valid(self, form):
+        current_tenant = CustomUser.objects.get(pk=self.kwargs['tenant_id'])
+        form.instance.tenant = current_tenant
+        form.instance.balance = form.instance.amount
+        form.instance.type = Payment.OFFLINE
+        form.instance.stripe_payment_id = None
+        return super().form_valid(form)
